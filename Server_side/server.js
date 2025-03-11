222b35e5851042bfb2dc6c4871d101be39e9e973
Isaac0603 <m.t.isaac414@gmail.com>
2025-03-11 22:32:04 +0530

    Update server.js

--- Server_side/server.js
+++ Server_side/server.js
@@ -29,7 +29,6 @@ app.use((req, res, next) => {
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
 })
 .then(() => console.log("✅ Connected to MongoDB"))
 .catch(err => {
