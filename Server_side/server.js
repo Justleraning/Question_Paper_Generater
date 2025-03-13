8dea277ccb961c1493f65168ee0abdd8fed0175b
Isaac0603 <m.t.isaac414@gmail.com>
2025-03-13 10:45:40 +0530

    Update server.jstest

--- Server_side/server.js
+++ Server_side/server.js
@@ -10,7 +10,11 @@ const app = express();
 
 // CORS Configuration
 app.use(cors({
-  origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+  origin: [
+    "http://localhost:3000", 
+    "http://127.0.0.1:3000",
+    "https://gilded-sherbet-4ee762.netlify.app" // Add your Netlify domain
+  ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
